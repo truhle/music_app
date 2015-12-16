30d0eb69b267389e1cfbc68e82d1c698f6436f40
Todd Ruhlen <rhythmtome@yahoo.com>
2015-12-15 20:36:31 -0500

    Add 'ugly lyrics' helper to format lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    lyrics = sanitize(lyrics)
+    lyrics_ary = lyrics.split("\n").map {|line| line.length > 1 ? "&#9835 " + line : line}
+    formatted_lyrics = "<pre>" + lyrics_ary.join + "</pre>"
+    formatted_lyrics.html_safe
+  end
 end

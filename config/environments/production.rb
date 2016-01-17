fac6125b7e9c0fbdcf155d7521f8c02c78f02f1e
Todd Ruhlen <rhythmtome@yahoo.com>
2016-01-16 21:26:37 -0500

    Old config again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.perform_deliveries = true
+  #
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",

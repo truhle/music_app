14928c4be8a9fc74696c92a02603a82a35fe3d82
Todd Ruhlen <rhythmtome@yahoo.com>
2016-01-16 21:09:28 -0500

    Add more mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
